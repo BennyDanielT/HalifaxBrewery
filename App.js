05c333ac19155f609f57022e86ebc9f817a7f63a
Benny Daniel <benny28dany@gmail.com>
2022-08-16 15:37:36 -0300

    conflicts

--- App.js
+++ App.js
@@ -1,34 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-const Stack = createNativeStackNavigator();
-// import Home from './screens/home.js';
-// import Add from './screens/add.js';
-// import Update from './screens/update.js';
-// import ViewData from './screens/view.js';
-// import Routing from './routes/routeMap.js';
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='home'
-          component={Home}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name='add' component={Add} />
-        <Stack.Screen name='update' component={Update} />
-        {/* <Stack.Screen name='view' component={ViewData} /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F45678',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
