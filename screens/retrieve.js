2644db432f2401afe9f1034f528878303f4ee7a9
Benny Daniel <benny28dany@gmail.com>
2022-08-25 01:16:30 -0300

    Updated Image in retrieve-data page and rendered data in Text Components

--- screens/retrieve.js
+++ screens/retrieve.js
@@ -13,35 +13,58 @@ import {
 } from 'react-native';
 // import React, { Component } from 'react';
 
-const goForFetch = () => {
-  fetch('https://atlanticbreweries.herokuapp.com/retrieve-data')
-    .then((response) => response.json())
-    .then((responseJson) => {
-      console.log('getting data from fetch', responseJson);
-    })
-    .catch((error) => console.log(error));
-};
-
 export default Retrieve = ({ navigation }) => {
+  const [products, setproducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const goForFetch = () => {
+    fetch('https://atlanticbreweries.herokuapp.com/retrieve-data')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        // console.log('getting data from fetch', responseJson);
+        setproducts(responseJson.inventoryData);
+        setIsLoading(false);
+        console.log(products[0]);
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <ScrollView>
-      <View>
-        <Text>Some more text</Text>
+      <View style={{ alignItems: 'center', flex: 1 }}>
+        {/* <Text>Some more text</Text> */}
         <Image
           source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+            uri: 'https://banner2.cleanpng.com/20180309/ajq/kisspng-beer-champagne-wine-alcoholic-drink-drinking-man-5aa29afd1db550.3834319415206059491217.jpg',
           }}
           style={{ width: 200, height: 200 }}
         />
+        <Text> {'\n'}</Text>
+        {isLoading && (
+          <Text>Data is being retrieved, wait pannu da ***ni =)</Text>
+        )}
       </View>
-      <TextInput
+      <View>
+        {products.map((prod, index) => (
+          <View key={prod._id}>
+            <Text> {'\n'}</Text>
+            <Text>{prod.brand}</Text>
+            <Text>{prod.facings}</Text>
+            <Text>{prod.logDate}</Text>
+            <Text>{prod.product}</Text>
+            <Text>{prod.province}</Text>
+            <Text>{prod.region}</Text>
+            <Text>{prod.store}</Text>
+          </View>
+        ))}
+      </View>
+      {/* <TextInput
         style={{
           height: 40,
           borderColor: 'gray',
           borderWidth: 1,
         }}
         defaultValue='You can type in me'
-      />
+      /> */}
       <Button title={'Click using Fetch'} onPress={goForFetch} color='green' />
       {/* {
         <FlatList
